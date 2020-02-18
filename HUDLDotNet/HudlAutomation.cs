efcefa08e7263dad72f23e833bffce2ab4390f23
David Handlos <David.Handlos@nelnet.net>
2020-02-18 16:09:46 -0600

    Documentation Update

--- HUDLDotNet/HudlAutomation.cs
+++ HUDLDotNet/HudlAutomation.cs
@@ -9,13 +9,26 @@ using OpenQA.Selenium.Support.UI;
 
 namespace NUnitHUDL
 {
+    /*
+     Class: HUDLBasic
+
+     Description: As the name describes, this is a very simple set of selenium-based tests to access the HUDL site.
+     These are strictly tests that each focus on logging into the HUDL site, and each querying a specific property/node on the page to ensure the login was valid.
+     
+     Notes: Beyond ensuring that specific content on the page exists, ideally the tests should be data driven, not statically entered into the tests as below.
+     This will make the tests less fragile and easier to maintain as the code changes.
+     */
+
     [TestFixture]
     public class HUDLBasic
     {
+        //Essential global variables, used across the tests
         private IWebDriver driver;
         private string UserEmail = "handlos@gmail.com";
         private string UserPass = "7980HUDL";
 
+        //Global "setup" function that happens at the beginning of each test.
+        //A Chrome browser is initialized, implict waits are set to 30 seconds, and the window is maximized.
         [SetUp]
         public void SetupTest()
         {
@@ -24,48 +37,68 @@ namespace NUnitHUDL
             driver.Manage().Window.Maximize();
         }
 
+        //Global "teardown" to kill the instance of the Chrome driver to avoid issues from having the open window to stay open
         [TearDown]
         public void TeardownTest()
         {
             driver.Quit();
         }
 
-        [Test]
-        public void HUDLBasicLogin_EmailExists()
+        //Basic "LoginSteps" method to repeat the same login steps per test to avoid redundant code
+        //Note: The "Wait" step was added to give the main page time to load, but this should normally be avoided by doing an explicit wait instead.
+        public void LoginSteps(string user, string pass)
         {
             driver.Navigate().GoToUrl("https://www.hudl.com/");
 
             driver.FindElement(By.LinkText("Log in")).Click();
             driver.FindElement(By.Id("email")).Clear();
-            driver.FindElement(By.Id("email")).SendKeys(UserEmail);
+            driver.FindElement(By.Id("email")).SendKeys(user);
             driver.FindElement(By.Id("password")).Clear();
-            driver.FindElement(By.Id("password")).SendKeys(UserPass);
+            driver.FindElement(By.Id("password")).SendKeys(pass);
             driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
-
             Wait(5000);
 
-            string PageEmail = driver.FindElement(By.XPath("//div[@class='hui-globaluseritem__email']")).GetAttribute("innerHTML");
+        }
+        public void Wait(int WaitMilliseconds)
+        {
+            System.Threading.Thread.Sleep(WaitMilliseconds);
+        }
+
+        //HUDLBasicLogin_EmailExists - Logs into the HUDL page, and then confirms the correct email address exists.
+        [Test]
+        public void HUDLBasicLogin_EmailExists()
+        {
+            LoginSteps(UserEmail, UserPass);
+
+            string PageContent = driver.FindElement(By.XPath("//div[@class='hui-globaluseritem__email']")).GetAttribute("innerHTML");
             bool TestPass = false;
-            if (PageEmail == UserEmail)
+            if (PageContent == UserEmail)
             {
                 TestPass = true;
             }
             Assert.IsTrue(TestPass);
         }
 
+        //HUDLBasicLogin_DisplayNameExists - Logs into the HUDL page, and then confirms the correct email address exists.
+        [Test]
+        public void HUDLBasicLogin_DisplayNameExists()
+        {
+            LoginSteps(UserEmail, UserPass);
+
+            string PageContent = driver.FindElement(By.XPath("//div[@class='hui-globaluseritem__display-name']/span")).GetAttribute("innerHTML");
+            bool TestPass = false;
+            if (PageContent == "David H")
+            {
+                TestPass = true;
+            }
+            Assert.IsTrue(TestPass);
+        }
+
+        //HUDLBasicLogin_TeamNameExists - Logs into the HUDL page, and then confirms the "QA Meetings" team name is being applied.
         [Test]
         public void HUDLBasicLogin_TeamNameExists()
         {
-            driver.Navigate().GoToUrl("https://www.hudl.com/");
-
-            driver.FindElement(By.LinkText("Log in")).Click();
-            driver.FindElement(By.Id("email")).Clear();
-            driver.FindElement(By.Id("email")).SendKeys(UserEmail);
-            driver.FindElement(By.Id("password")).Clear();
-            driver.FindElement(By.Id("password")).SendKeys(UserPass);
-            driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
-
-            Wait(5000);
+            LoginSteps(UserEmail, UserPass);
 
             string PageContent = driver.FindElement(By.XPath("//div[@class='hui-primaryteamswitcher__display-name']/span")).GetAttribute("innerHTML");
             bool TestPass = false;
@@ -76,20 +109,27 @@ namespace NUnitHUDL
             Assert.IsTrue(TestPass);
         }
 
+
+        [Test]
+        public void HUDLBasicLogin_UploadExists()
+        {
+            LoginSteps(UserEmail, UserPass);
+
+            string PageContent = driver.FindElement(By.XPath("//a[@class='hui-globalnav__upload hui-globalnav__upload--button-display uni-btn--secondary uni-btn--small']/span")).Text;
+            bool TestPass = false;
+            if (PageContent == "Upload")
+            {
+                TestPass = true;
+            }
+            Assert.IsTrue(TestPass);
+        }
+
+        //HUDLBasicLogin_BadPassword - Basic bad path test to confirm that using a bad user name will result in the expected error message on the page.
         [Test]
         public void HUDLBasicLogin_BadUser()
         {
             string BadUserEmail = "me@me.com";
-            driver.Navigate().GoToUrl("https://www.hudl.com/");
-
-            driver.FindElement(By.LinkText("Log in")).Click();
-            driver.FindElement(By.Id("email")).Clear();
-            driver.FindElement(By.Id("email")).SendKeys(BadUserEmail);
-            driver.FindElement(By.Id("password")).Clear();
-            driver.FindElement(By.Id("password")).SendKeys(UserPass);
-            driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
-
-            Wait(5000);
+            LoginSteps(BadUserEmail, UserPass);
 
             string PageContent = driver.FindElement(By.XPath("//div[@class='login-error-container']/p")).GetAttribute("innerHTML");
             bool TestPass = false;
@@ -100,20 +140,12 @@ namespace NUnitHUDL
             Assert.IsTrue(TestPass);
         }
 
+        //HUDLBasicLogin_BadPassword - Basic bad path test to confirm that using a bad password will result in the expected error message on the page.
         [Test]
         public void HUDLBasicLogin_BadPassword()
         {
             string BadUserPass = "7980HUDLBad";
-            driver.Navigate().GoToUrl("https://www.hudl.com/");
-
-            driver.FindElement(By.LinkText("Log in")).Click();
-            driver.FindElement(By.Id("email")).Clear();
-            driver.FindElement(By.Id("email")).SendKeys(UserEmail);
-            driver.FindElement(By.Id("password")).Clear();
-            driver.FindElement(By.Id("password")).SendKeys(BadUserPass);
-            driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
-
-            Wait(5000);
+            LoginSteps(UserEmail, BadUserPass);
 
             string PageContent = driver.FindElement(By.XPath("//div[@class='login-error-container']/p")).GetAttribute("innerHTML");
             bool TestPass = false;
@@ -128,33 +160,6 @@ namespace NUnitHUDL
 
 
 
-        public void Wait(int WaitMilliseconds)
-        {
-            System.Threading.Thread.Sleep(WaitMilliseconds);
-        }
-
-        public void ElementClick(IWebElement elem)
-        {
-            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            //  waitForElement.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(elem));
-            //            waitForElement.Until(ExpectedConditions.ElementExists(By.XPath(button)));
-            //Browser.Instance.FindElement(By.XPath(button)).Click();
-            elem.Click();
-        }
-
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
 
 
     }
